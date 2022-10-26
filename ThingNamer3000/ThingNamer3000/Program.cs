bc2bf5fb406ffe2e0b74d97ac27a76e525ae50de
Sean McNeil <smcneil136@gmail.com>
2022-10-26 09:52:39 -0400

    Thing Namer 3000 Challenge

---
+++ ThingNamer3000/ThingNamer3000/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine(); // This is the thing being named, received from the user
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine(); // This is what describes the named thing, received from the user
+string c = "of Doom"; // DOOM
+string d = "3000"; // Gotta have a number
+// Removed second "of" that was displaying between a and c and changed it to just a space
+Console.WriteLine("The " + b + " " + a + " " + c + " " + d + "!");
+/* Aside from commenting, assigning more descriptive names to the variables can help with code readability */
