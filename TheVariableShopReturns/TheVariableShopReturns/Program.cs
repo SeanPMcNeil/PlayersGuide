ad26384c54799392205a572270d114a2ca1cb0aa
Sean McNeil <smcneil136@gmail.com>
2022-10-27 13:22:20 -0400

    The Variable Shop Returns Challenge

---
+++ TheVariableShopReturns/TheVariableShopReturns/Program.cs
@@ -0,0 +1,83 @@
+﻿char aLetter = 'a';
+Console.WriteLine(aLetter);
+
+bool aBoolean = true;
+Console.WriteLine(aBoolean);
+
+byte aByte = 56;
+Console.WriteLine(aByte);
+
+sbyte aSByte = -46;
+Console.WriteLine(aSByte);
+
+ushort anUShort = 45897;
+Console.WriteLine(anUShort);
+
+short aShort = 14576;
+Console.WriteLine(aShort);
+
+uint anUInt = 3_178_874_346;
+Console.WriteLine(anUInt);
+
+int anInt = 876432;
+Console.WriteLine(anInt);
+
+ulong aBigNumber = 243987293874UL;
+Console.WriteLine(aBigNumber);
+
+long aBigNegativeNumber = -329817938279356871L;
+Console.WriteLine(aBigNegativeNumber);
+
+float aFloat = 3.141529F;
+Console.WriteLine(aFloat);
+
+double aDouble = 3.141925;
+Console.WriteLine(aDouble);
+
+decimal aDecimal = 45.9871298638175M;
+Console.WriteLine(aDecimal);
+
+string aString = "This is an example of a string!";
+Console.WriteLine(aString);
+
+aLetter = 'b';
+Console.WriteLine(aLetter);
+
+aBoolean = false;
+Console.WriteLine(aBoolean);
+
+aByte = 76;
+Console.WriteLine(aByte);
+
+aSByte = -67;
+Console.WriteLine(aSByte);
+
+anUShort = 34576;
+Console.WriteLine(anUShort);
+
+aShort = -14876;
+Console.WriteLine(aShort);
+
+anUInt = 9823764;
+Console.WriteLine(anUInt);
+
+anInt = 867534;
+Console.WriteLine(anInt);
+
+aBigNumber = 987612876547812364UL;
+Console.WriteLine(aBigNumber);
+
+aBigNegativeNumber = -98712634876234L;
+Console.WriteLine(aBigNegativeNumber);
+
+aFloat = 2.87162F;
+Console.WriteLine(aFloat);
+
+aDouble = 5.9876134;
+Console.WriteLine(aDouble);
+
+aDecimal = 2.98273894872893847M;
+Console.WriteLine(aDecimal);
+
+aString = "Now this is a new string!";
+Console.WriteLine(aString);
