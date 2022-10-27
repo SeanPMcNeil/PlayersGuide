f8318da6e35886c60906b7ffc39279e42d5b5a06
Sean McNeil <smcneil136@gmail.com>
2022-10-27 13:12:59 -0400

    The Variable Shop Challenge

---
+++ TheVariableShop/TheVariableShop/Program.cs
@@ -0,0 +1,41 @@
+﻿char aLetter = 'a';
+Console.WriteLine(aLetter);
+
+bool aBoolean = true;
+Console.WriteLine(aBoolean);
+
+byte aByte = 56;
+Console.WriteLine(aByte);
+
+sbyte aSByte = -46;
+Console.WriteLine(aSByte);
+
+ushort anUShort = 45897;
+Console.WriteLine(anUShort);
+
+short aShort = 14576;
+Console.WriteLine(aShort);
+
+uint anUInt = 3_178_874_346;
+Console.WriteLine(anUInt);
+
+int anInt = 876432;
+Console.WriteLine(anInt);
+
+ulong aBigNumber = 243987293874UL;
+Console.WriteLine(aBigNumber);
+
+long aBigNegativeNumber = -329817938279356871L;
+Console.WriteLine(aBigNegativeNumber);
+
+float aFloat = 3.141529f;
+Console.WriteLine(aFloat);
+
+double aDouble = 3.141925;
+Console.WriteLine(aDouble);
+
+decimal aDecimal = 45.9871298638175M;
+Console.WriteLine(aDecimal);
+
+string aString = "This is an example of a string!";
+Console.WriteLine(aString);
