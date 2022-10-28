63229c97ab9a9e681268fcde186e93ef23179be2
Sean McNeil <smcneil136@gmail.com>
2022-10-28 13:45:57 -0400

    The Dominion of Kings Challenge

---
+++ TheDominionofKings/TheDominionofKings/Program.cs
@@ -0,0 +1,18 @@
+﻿int score;
+
+Console.WriteLine("How many estates do you have?");
+int estates = Convert.ToInt32(Console.ReadLine());
+
+score = estates;
+
+Console.WriteLine("How many duchies do you have?");
+int duchies = Convert.ToInt32(Console.ReadLine());
+
+score += (duchies * 3);
+
+Console.WriteLine("How many provinces do you have?");
+int provinces = Convert.ToInt32(Console.ReadLine());
+
+score += (provinces * 6);
+
+Console.WriteLine($"Total Score: {score}");
