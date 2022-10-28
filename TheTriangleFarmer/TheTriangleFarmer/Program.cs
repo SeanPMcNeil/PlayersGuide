b47eee1cdc264e716e886cc407c92e5d6ccc31e2
Sean McNeil <smcneil136@gmail.com>
2022-10-28 12:17:12 -0400

    The Triangle Farmer Challenge

---
+++ TheTriangleFarmer/TheTriangleFarmer/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Please enter a base length of the triangle (decimals allowed): ");
+double triangleBase = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Please enter the height of the triangle (decimals allowed): ");
+double triangleHeight = Convert.ToDouble(Console.ReadLine());
+
+double triangleArea = (triangleBase * triangleHeight) / 2;
+Console.WriteLine($"The area of the triangle is: {triangleArea}");
