bd586e577922ba3298a2f77abde7f95948374e22
Sean McNeil <smcneil136@gmail.com>
2022-11-03 16:00:50 -0400

    Taking a Number Challenge

---
+++ TakingANumber/TakingANumber/Program.cs
@@ -0,0 +1,31 @@
+﻿int result = AskForNumber("What is the airspeed velocity of an unladen swallow?");
+
+Console.WriteLine(result);
+
+int result2 = AskForNumberInRange("What is the airspeed velocity of an unladen swallow?", 23, 25);
+
+Console.WriteLine(result2);
+
+int AskForNumber(string text)
+{
+    Console.Write(text + " ");
+    string? response = Console.ReadLine();
+    int number = Convert.ToInt32(response);
+
+    return number;
+}
+
+int AskForNumberInRange(string text, int min, int max)
+{
+    while (true)
+    {
+        Console.Write(text + " ");
+        string? response = Console.ReadLine();
+        int number = Convert.ToInt32(response);
+
+        if (number >= min && number <= max)
+            return number;
+
+        Console.WriteLine("Guess again!");
+    }
+}
