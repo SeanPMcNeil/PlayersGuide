88b504e29eb2a1cc0e386862e0e93e3ca325d7b4
Sean McNeil <smcneil136@gmail.com>
2022-11-03 12:10:50 -0400

    The Laws of Freach Challenge

---
+++ TheLawsOfFreach/TheLawsOfFreach/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+int currentSmallest = int.MaxValue; // Start higher than anything in the array.
+foreach (int number in array)
+{
+    if (number < currentSmallest)
+        currentSmallest = number;
+}
+Console.WriteLine(currentSmallest);
+
+
+int total = 0;
+foreach (int number in array)
+    total += number;
+float average = (float)total / array.Length;
+Console.WriteLine(average);
