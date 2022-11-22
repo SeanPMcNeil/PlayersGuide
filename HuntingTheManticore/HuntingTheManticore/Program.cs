a58cd840690baf0ccd9a3b276236756f27be7c47
Sean McNeil <smcneil136@gmail.com>
2022-11-22 10:22:13 -0500

    Hunting the Manticore - message colors added

--- HuntingTheManticore/HuntingTheManticore/Program.cs
+++ HuntingTheManticore/HuntingTheManticore/Program.cs
@@ -19,6 +19,8 @@ int round = 1;
 
 do
 {
+    Console.ForegroundColor = ConsoleColor.White;
+
     Console.WriteLine("-----------------------------------------------------------");
     Console.WriteLine($"STATUS: Round: {round}  City: {cityHealth}/15  Manticore: {manticoreHealth}/10");
 
@@ -26,14 +28,22 @@ do
 
     if (round % 3 == 0 && round % 5 == 0)
     {
+        Console.ForegroundColor = ConsoleColor.Blue;
         cannonDamage = 10;
     }
-    else if (round % 3 == 0 || round % 5 == 0)
+    else if (round % 3 == 0)
     {
+        Console.ForegroundColor = ConsoleColor.Red;
+        cannonDamage = 3;
+    }
+    else if (round % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
         cannonDamage = 3;
     }
 
     Console.WriteLine($"The cannon is expected to deal {cannonDamage} damage this round.");
+    Console.ForegroundColor = ConsoleColor.White;
 
     Console.Write("Enter desired cannon range: ");
     string? playerTwoResponse = Console.ReadLine();
@@ -42,6 +52,7 @@ do
 
     if (cannonDistance == manticoreDistance)
     {
+        Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("That round was a DIRECT HIT!");
         manticoreHealth -= cannonDamage;
         if (manticoreHealth > 0)
@@ -50,12 +61,14 @@ do
     }
     else if (cannonDistance < manticoreDistance)
     {
+        Console.ForegroundColor = ConsoleColor.Magenta;
         Console.WriteLine("That round FELL SHORT of the target.");
         cityHealth--;
         round++;
     }
     else
     {
+        Console.ForegroundColor = ConsoleColor.Gray;
         Console.WriteLine("That round OVERSHOT the target.");
         cityHealth--;
         round++;
