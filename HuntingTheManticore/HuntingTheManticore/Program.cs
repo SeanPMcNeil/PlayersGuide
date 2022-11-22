5fee45fe1a52c5302d09592af87795bff04ec6ba
Sean McNeil <smcneil136@gmail.com>
2022-11-22 10:14:09 -0500

    Hunting the Manticore Challenge (need to add methods)

--- HuntingTheManticore/HuntingTheManticore/Program.cs
+++ HuntingTheManticore/HuntingTheManticore/Program.cs
@@ -15,7 +15,54 @@ while (manticoreDistance < 0 || manticoreDistance > 100)
 
 Console.Clear();
 
-while (manticoreHealth > 0 || cityHealth > 0)
+int round = 1;
+
+do
 {
-    
-}
+    Console.WriteLine("-----------------------------------------------------------");
+    Console.WriteLine($"STATUS: Round: {round}  City: {cityHealth}/15  Manticore: {manticoreHealth}/10");
+
+    int cannonDamage = 1;
+
+    if (round % 3 == 0 && round % 5 == 0)
+    {
+        cannonDamage = 10;
+    }
+    else if (round % 3 == 0 || round % 5 == 0)
+    {
+        cannonDamage = 3;
+    }
+
+    Console.WriteLine($"The cannon is expected to deal {cannonDamage} damage this round.");
+
+    Console.Write("Enter desired cannon range: ");
+    string? playerTwoResponse = Console.ReadLine();
+    int cannonDistance = Convert.ToInt32(playerTwoResponse);
+
+
+    if (cannonDistance == manticoreDistance)
+    {
+        Console.WriteLine("That round was a DIRECT HIT!");
+        manticoreHealth -= cannonDamage;
+        if (manticoreHealth > 0)
+            cityHealth--;
+        round++;
+    }
+    else if (cannonDistance < manticoreDistance)
+    {
+        Console.WriteLine("That round FELL SHORT of the target.");
+        cityHealth--;
+        round++;
+    }
+    else
+    {
+        Console.WriteLine("That round OVERSHOT the target.");
+        cityHealth--;
+        round++;
+    }
+} while (manticoreHealth > 0 && cityHealth > 0);
+
+if (manticoreHealth <= 0)
+    Console.WriteLine("The Manticore has been destroyed! The city of Consolas has been saved!");
+else if (cityHealth <= 0)
+    Console.WriteLine("The Manticore has destroyed the city of Consolas!");
